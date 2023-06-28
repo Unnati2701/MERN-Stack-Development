4c906865e7fe93a87b2318a62e7f764053f75b31
Unnati2701 <unnati012@gmail.com>
2023-06-28 15:28:30 +0530

    get

--- Section 3/backend/router/userRouter.js
+++ Section 3/backend/router/userRouter.js
@@ -14,20 +14,42 @@ router.post('/add', (req, res) => {
     });
 });
 
+router.get('/getall', (req, res) => {
+    Model.find({})
+    .then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        res.status(500).json(err);
+    });
+});
+
 router.get('/delete', (req, res) => {
     res.send('Response from  user delete server');
 });
 
-router.get('/getbyid', (req, res) => {
-    res.send('Response from  user get by id server');
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.params.id)
+    .then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        res.status(500).json(err);
+    });
 });
 
 router.get('/update', (req, res) => {
     res.send('Response from  user update server');
 });
 
-router.get('/getbyemail', (req, res) => {
-    res.send('Response from  user get by email server');
+router.get('/getbyemail/:email', (req, res) => {
+
+    console.log(req.params.email);
+
+    Model.findOne({email: req.params.email })
+    .then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        res.status(500).json(err);
+        });
 });
 
 module.exports = router; // export router
